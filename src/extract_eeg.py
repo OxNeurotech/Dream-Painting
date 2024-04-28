73371200f6994a1f98c6305ebab04d3f04d18b64
Lee Chih Jung <54738966+LordJatonyas@users.noreply.github.com>
2024-04-28 13:28:50 +0100

    Didn't manage to submit

--- src/extract_eeg.py
+++ src/extract_eeg.py
@@ -1,6 +1,5 @@
 # This script extracts just the EEG data from the .pth dataset
 # and moves this into a new dataset file (.csv extension)
-
 import os
 import torch
 from typing import List
@@ -26,30 +25,23 @@ def standardize_eeg_sample(eeg: List[float]) -> List[float]:
 # Channel Numbers
 
 # get the channel numbers in order
-file = open(os.path.join(data_dir, "channel_names.txt"))
+file = open("../data/channel_names.txt")
 channel_names = file.read().splitlines()
 file.close()
 
-fieldnames = ["image"]
+fieldnames = ["subject", "label"]
 fieldnames.extend(channel_names)
 
-subj_1 = dict.fromkeys(fieldnames, [])
-subj_2 = dict.fromkeys(fieldnames, [])
-subj_3 = dict.fromkeys(fieldnames, [])
-subj_4 = dict.fromkeys(fieldnames, [])
-subj_5 = dict.fromkeys(fieldnames, [])
-subj_6 = dict.fromkeys(fieldnames, [])
-
-subj_choices = {1: subj_1, 2: subj_2, 3: subj_3, 4: subj_4, 5: subj_5, 6: subj_6}
-electrode_pos = dict(zip(range(128), channel_names))
+eeg_data = {key: [] for key in dict.fromkeys(fieldnames)}
 
 # load data into dictionaries
+
 print("Loading data...")
 for i in tqdm(range(len(model['dataset']))):
-    subj = subj_choices[model['dataset'][i]['subject']]
-    subj['image'].append(model['dataset'][i]['image'])
-    for j in range(len(model['dataset'][i]['eeg'])):
-        subj[electrode_pos[j]].append(standardize_eeg_sample(model['dataset'][i]['eeg'][j]))
+    eeg_data['subject'].append(model['dataset'][i]['subject'])
+    eeg_data['label'].append(model['dataset'][i]['label'])
+    for j in range(128):
+        eeg_data[channel_names[j]].append(standardize_eeg_sample(model['dataset'][i]['eeg'][j]))
 print("Data loaded!")
 
-torch.save(subj_choices, os.path.join(data_dir, "data.pth"))
+torch.save(eeg_data, os.path.join(data_dir, "data.pt"))
